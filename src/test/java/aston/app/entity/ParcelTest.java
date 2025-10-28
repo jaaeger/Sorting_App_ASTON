0d3f1e26148d4e0fa7ae66c5261bab0de7a10710
HalievJanat <152373700+HalievJanat@users.noreply.github.com>
2025-10-28 21:38:58 +0400

    Fixed test Parcel

--- src/test/java/aston/app/entity/ParcelTest.java
+++ src/test/java/aston/app/entity/ParcelTest.java
@@ -2,9 +2,13 @@ package aston.app.entity;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,56 +18,104 @@ public class ParcelTest {
     @BeforeEach
     public void setUp() {
         data = new HashMap<>();
-        data.put("recipientName", "user1");
+        data.put("recipientName", "user");
         data.put("weight", "12.5");
         data.put("trackingNumber", "5323");
     }
 
-    @Test
-    public void when_CreateStandardParcelWithMaxDimension_then_ParcelCreated() {
-        data.put("type", "STANDARD");
-        data.put("maxDimension", "200");
+    @Nested
+    @DisplayName("Positive case")
+    class PositiveCase {
+        @Test
+        public void when_CreateStandardParcelWithMaxDimension_then_ParcelCreated() {
+            data.put("type", "STANDARD");
+            data.put("maxDimension", "200");
 
-        StandardParcel parcel = (StandardParcel) Parcel.createFromMap(data);
+            StandardParcel parcel = (StandardParcel) Parcel.createFromMap(data);
 
-        Assertions.assertEquals("user1", parcel.recipientName);
-        Assertions.assertEquals(12.5, parcel.weight);
-        Assertions.assertEquals(5323, parcel.trackingNumber);
-        Assertions.assertEquals(200, parcel.getMaxDimension());
+            Assertions.assertEquals("user", parcel.recipientName);
+            Assertions.assertEquals(12.5, parcel.weight);
+            Assertions.assertEquals(5323, parcel.trackingNumber);
+            Assertions.assertEquals(200, parcel.getMaxDimension());
+        }
+
+        @Test
+        public void when_CreateExpressParcelWithDeliveryDeadline_then_ExpressParcelCreated() {
+            data.put("type", "EXPRESS");
+            data.put("deliveryDeadline", "2024-01-15");
+
+            ExpressParcel parcel = (ExpressParcel) Parcel.createFromMap(data);
+
+            Assertions.assertEquals("user", parcel.recipientName);
+            Assertions.assertEquals(12.5, parcel.weight);
+            Assertions.assertEquals(5323, parcel.trackingNumber);
+            Assertions.assertEquals(LocalDate.of(2024, 1, 15), parcel.getDeliveryDeadline());
+        }
+
+        @Test
+        public void when_CreateInternationalParcelWithDestinationCountry_then_InternationalParcelCreated() {
+            data.put("type", "INTERNATIONAL");
+            data.put("destinationCountry", "Англия");
+
+            InternationalParcel parcel = (InternationalParcel) Parcel.createFromMap(data);
+
+            Assertions.assertEquals("user", parcel.recipientName);
+            Assertions.assertEquals(12.5, parcel.weight);
+            Assertions.assertEquals(5323, parcel.trackingNumber);
+            Assertions.assertEquals("Англия", parcel.getDestinationCountry());
+        }
     }
 
-    @Test
-    public void when_CreateExpressParcelWithDeliveryDeadline_then_ExpressParcelCreated() {
-        data.put("type", "EXPRESS");
-        data.put("deliveryDeadline", "2024-01-15");
+    @Nested
+    @DisplayName("Negative case")
+    class NegativeCase {
+        @Test
+        void when_CreateParcelWithInvalidType_then_ThrowException() {
+            data.put("type", "UNKNOWN_TYPE");
 
-        ExpressParcel parcel = (ExpressParcel) Parcel.createFromMap(data);
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Parcel.createFromMap(data);
+            });
+        }
 
-        Assertions.assertEquals("user1", parcel.recipientName);
-        Assertions.assertEquals(12.5, parcel.weight);
-        Assertions.assertEquals(5323, parcel.trackingNumber);
-        Assertions.assertEquals(LocalDate.of(2024, 1, 15), parcel.getDeliveryDeadline());
-    }
+        @Test
+        void when_CreateParcelWithInvalidWeight_then_ThrowException() {
+            data.put("type", "INTERNATIONAL");
+            data.put("weight", "svas");
 
-    @Test
-    public void when_CreateInternationalParcelWithDestinationCountry_then_InternationalParcelCreated() {
-        data.put("type", "INTERNATIONAL");
-        data.put("destinationCountry", "Англия");
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Parcel.createFromMap(data);
+            });
+        }
 
-        InternationalParcel parcel = (InternationalParcel) Parcel.createFromMap(data);
+        @Test
+        void when_CreateParcelWithInvalidMaxDimension_then_ThrowException() {
+            data.put("type", "STANDARD");
+            data.put("maxDimension", "svas");
 
-        Assertions.assertEquals("user1", parcel.recipientName);
-        Assertions.assertEquals(12.5, parcel.weight);
-        Assertions.assertEquals(5323, parcel.trackingNumber);
-        Assertions.assertEquals("Англия", parcel.getDestinationCountry());
-    }
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Parcel.createFromMap(data);
+            });
+        }
 
-    @Test
-    void when_CreateParcelWithInvalidType_then_ThrowException() {
-        data.put("type", "UNKNOWN_TYPE");
+        @Test
+        void when_CreateParcelWithInvalidTrackingNumber_then_ThrowException() {
+            data.put("type", "INTERNATIONAL");
+            data.put("trackingNumber", "svas");
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Parcel.createFromMap(data);
-        });
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                Parcel.createFromMap(data);
+            });
+        }
+
+        @Test
+        void when_CreateParcelWithInvalidDeliveryDeadline_then_ThrowException() {
+            data.put("type", "EXPRESS");
+            data.put("deliveryDeadline", "svas");
+
+            Assertions.assertThrows(DateTimeParseException.class, () -> {
+                Parcel.createFromMap(data);
+            });
+        }
     }
 }
