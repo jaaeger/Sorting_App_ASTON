065a4bd9eeaae5cbe245e416da6c08fdcc63c31e
jaaeger <iwan.egerev@yandex.ru>
2025-10-28 18:43:19 +0300

    upd

--- src/main/java/aston/app/App.java
+++ src/main/java/aston/app/App.java
@@ -1,19 +1,14 @@
 package aston.app;
 
-import aston.app.core.comparator.ParcelComparators;
 import aston.app.entity.ExpressParcel;
 import aston.app.entity.InternationalParcel;
 import aston.app.entity.Parcel;
 import aston.app.entity.StandardParcel;
-import aston.app.input.model.NameField;
 import aston.app.ui.ConsoleUI;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -25,17 +20,7 @@ public class App {
         list.add(new StandardParcel("Галина", 4.1, 5, 75));
         list.add(new InternationalParcel("Дмитрий", 0.9, 500, "DE"));
 
-        Map<NameField, Comparator<? super Parcel>> comparators = new EnumMap<>(NameField.class);
-
-        comparators.put(NameField.RECIPIENT_NAME, ParcelComparators.byRecipientName());
-        comparators.put(NameField.WEIGHT, ParcelComparators.byWeight());
-        comparators.put(NameField.TRACKING_NUMBER, ParcelComparators.byTrackingNumber());
-
-        comparators.put(NameField.MAX_DIMENSION, ParcelComparators.byMaxDimension());
-        comparators.put(NameField.DESTINATION_COUNTRY, ParcelComparators.byDestinationCountry());
-        comparators.put(NameField.DELIVERY_DEADLINE, ParcelComparators.byDeliveryDeadline());
-
-        ConsoleUI ui = new ConsoleUI(new Scanner(System.in), list, comparators);
+        ConsoleUI ui = new ConsoleUI(new Scanner(System.in), list);
         ui.run();
     }
 }
